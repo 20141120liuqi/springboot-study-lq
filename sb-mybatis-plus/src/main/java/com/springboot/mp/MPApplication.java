bef5f665fc7a83276f02c4c2e4c81e9f00fb5306
liuqi <liuqi001@foxmail.com>
2018-11-19 17:28:56 +0800

    first commit

---
+++ sb-mybatis-plus/src/main/java/com/springboot/mp/MPApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MPApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MPApplication.class,args);
+
+    }
+}
